a906d5b8e8f8d5000e52e31fb0740f3946b3294f
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2023-06-02 19:47:34 -0300

    preenchendo select

--- horas.js
+++ horas.js
@@ -3,6 +3,11 @@ function carregar() {
     var data = new Date()
     var hora = data.getHours()
     var minuto =  data.getMinutes()
+    var listar = document.querySelector('select#flista')
+    let vetores = [
+     'SE EU ERREI','DINHEIRO NÃO HÁ', 'GIGANTE', 'ILUMINADO',
+     'GOSTO DO SAMBA','ALDINHA','AMAR TODA HORA','LESSE','OCEANO','VENTOS', 'BRILHO DO SOL', 'ALMA PENADA', 'SAMBA DA PAZ', 'LAURINHA','VIVER VIVER','BRASIL', 'SILÊNCIO','FAKE NEWS', 'ESPERANÇA', 'COPANHEIRA',  
+     ]
     horas.innerHTML = `<h3>Agora são <strong> ${hora} horas e ${minuto} </strong>minutos</h3>`
     if (hora >= 0 && hora < 12) {
          horas.innerHTML +=`<h3>  Bom dia<h3>`
@@ -14,4 +19,13 @@ function carregar() {
          horas.innerHTML +=`<h3>  Boa Noite</h3>`
          document.body.style.background="rgb(243, 236, 236)"
     }
+     for (x in vetores) {
+         let item = document.createElement('option')
+         item.text = vetores[x]
+         listar.appendChild(item)
+     }
+    /* var opcaoTexto = select.options[select.selectedIndex].text;
+     var opcaoValor = select.options[select.selectedIndex].value;*/
+        
+
 }
