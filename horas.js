96a52a5545672bd22b39079e9d3e91c37f7dcc76
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2023-06-03 13:52:24 -0300

    escolhendo no  select

--- horas.js
+++ horas.js
@@ -1,13 +1,16 @@
-function carregar() {
-    var horas = document.getElementById('horas')
-    var data = new Date()
-    var hora = data.getHours()
-    var minuto =  data.getMinutes()
-    var listar = document.querySelector('select#flista')
+
+    let res = document.getElementById("res") 
+    let horas = document.getElementById('horas')
+    let data = new Date()
+    let hora = data.getHours()
+    let minuto =  data.getMinutes()
+    let listar = document.querySelector('select#flista')
     let vetores = [
      'SE EU ERREI','DINHEIRO NÃO HÁ', 'GIGANTE', 'ILUMINADO',
      'GOSTO DO SAMBA','ALDINHA','AMAR TODA HORA','LESSE','OCEANO','VENTOS', 'BRILHO DO SOL', 'ALMA PENADA', 'SAMBA DA PAZ', 'LAURINHA','VIVER VIVER','BRASIL', 'SILÊNCIO','FAKE NEWS', 'ESPERANÇA', 'COPANHEIRA',  
      ]
+function carregar() {
+    
     horas.innerHTML = `<h3>Agora são <strong> ${hora} horas e ${minuto} </strong>minutos</h3>`
     if (hora >= 0 && hora < 12) {
          horas.innerHTML +=`<h3>  Bom dia<h3>`
@@ -24,8 +27,17 @@ function carregar() {
          item.text = vetores[x]
          listar.appendChild(item)
      }
-    /* var opcaoTexto = select.options[select.selectedIndex].text;
-     var opcaoValor = select.options[select.selectedIndex].value;*/
+     document.getElementById("flista").value='SE EU ERREI'
+     
         
 
 }
+
+
+function pegar() {
+     
+     /*res.innerHTML += `<strong>elemento da select é ${listar.lastChild.text}</strong>`*/ 
+     res.innerHTML += `<strong>elemento da select é ${document.getElementById("flista").value}</strong>` 
+
+}
+
