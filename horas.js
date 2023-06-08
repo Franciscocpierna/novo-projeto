83ed3152697116275b7710901c5e21a3e67f51d7
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2023-06-08 19:49:21 -0300

    tentando esconder iframe

--- horas.js
+++ horas.js
@@ -1,18 +1,21 @@
-    let musica = document.getElementById('iframe#videos')  
-    let horas = document.getElementById('horas')
-    let data = new Date()
-    let hora = data.getHours()
-    let minuto =  data.getMinutes()
-    let listar = document.querySelector('select#flista')
+    let divisao = document.getElementById('contem');
+    let musica = document.getElementById('videos'); 
+    let horas = document.getElementById('horas');
+    let data = new Date();
+    let hora = data.getHours();
+    let minuto =  data.getMinutes();
+    let listar = document.querySelector('select#flista');
     let vetores = [
      'SE EU ERREI','DINHEIRO NÃO HÁ', 'GIGANTE', 'ILUMINADO',
      'GOSTO DO SAMBA','ALDINHA','AMAR TODA HORA','LESSE','OCEANO','VENTOS', 'BRILHO DO SOL', 'ALMA PENADA', 'SAMBA DA PAZ', 'LAURINHA','VIVER VIVER','BRASIL', 'SILÊNCIO','FAKE NEWS', 'ESPERANÇA', 'COPANHEIRA',  
-     ]
+     ];
 
-     atual= ""
-     
-
-function carregar() {
+    let atual= "";
+    let xy = 1;
+    let botao =document.getElementById('botao');
+    /*botao.addEventListener("click", executar());*/
+  
+    function carregar() {
     
     horas.innerHTML = `<h3>Agora são <strong> ${hora} horas e ${minuto} </strong>minutos</h3>`
     if (hora >= 0 && hora < 12) {
@@ -33,6 +36,8 @@ function carregar() {
      
      document.getElementById("flista").value='SE EU ERREI'
      atual = document.getElementById("flista").value
+     divisao.style.display = "none"
+     musica.style.display = "none"
 
    
 }
@@ -40,17 +45,50 @@ function carregar() {
 function executar() {
      
      
-    atual = document.getElementById("flista").value
+    atual = document.getElementById("flista").value;
     ifrm = document.createElement("iframe");
     ifrm.style.width =560+"px";
     ifrm.style.height =  315+"px";
+    
     /*document.getElementById("myDIV").style.display = "none";
     document.getElementById("myDIV").style.display = "none"; */
-    document.body.appendChild(ifrm);
-    document.getElementById('videos').style.display = "none"; 
    
+    if(xy==1){  
+      
+      document.body.appendChild(ifrm);
+      xy = 0;
+    } 
+     /*divisao.style.appendChild(ifrm);*/
+     
+    /*document.getElementById('videos').style.display = "none";*/ 
+    
+   invisivel();
 }
+ function invisivel(){
+     /*alert(`o valor de display ${musica.style.display}`);*/
+     if(musica.style.display  === "none"){
+          /*alert('entrou em === none');*/
+          musica.style.display  = "block"; 
+          document.getElementById('videos').hidden=false; 
+          document.body.ifrm.hidden = false;
+     } else {
+         /* alert('entrou em === block');*/
+          musica.style.display  = "none";
+          document.getElementById('iframe#videos').hidden=true;
+          musica.style.hidden  = true;  
+          document.body.ifrm.hidden = true;
+     }   
+    /* if(x==1) {
+          alert(`o valor de x ${x}`)
+       
+          document.body.style.getElementById('videos').display = "block" 
+     }else {
+          document.body.style.getElementById('videos').display = "none" 
+          alert(`o valor de x = ${x}`)
+     }*/   
+     /*document.getElementById('videos').style.display = "none";*/
 
+ }
 /*function tocar_video(mysrc){
 	var video = document.createElement('video');
 
