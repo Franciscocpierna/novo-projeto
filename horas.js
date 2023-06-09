52c34974a7e886f2e3e3096f63f81ab4d9568edd
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2023-06-09 19:49:02 -0300

    atual mudança de idéia

--- horas.js
+++ horas.js
@@ -1,5 +1,4 @@
-    let divisao = document.getElementById('contem');
-    let musica = document.getElementById('videos'); 
+    
     let horas = document.getElementById('horas');
     let data = new Date();
     let hora = data.getHours();
@@ -11,9 +10,9 @@
      ];
 
     let atual= "";
-    let xy = 1;
+    
     let botao =document.getElementById('botao');
-    /*botao.addEventListener("click", executar());*/
+    
   
     function carregar() {
     
@@ -34,11 +33,11 @@
          listar.appendChild(item)
      }
      
-     document.getElementById("flista").value='SE EU ERREI'
-     atual = document.getElementById("flista").value
-     divisao.style.display = "none"
-     musica.style.display = "none"
-
+     document.getElementById("flista").value='SE EU ERREI';
+     atual = document.getElementById("flista").value;
+     
+     
+     
    
 }
 
@@ -46,60 +45,8 @@ function executar() {
      
      
     atual = document.getElementById("flista").value;
-    ifrm = document.createElement("iframe");
-    ifrm.style.width =560+"px";
-    ifrm.style.height =  315+"px";
+
     
-    /*document.getElementById("myDIV").style.display = "none";
-    document.getElementById("myDIV").style.display = "none"; */
-   
-    if(xy==1){  
-      
-      document.body.appendChild(ifrm);
-      xy = 0;
-    } 
-     /*divisao.style.appendChild(ifrm);*/
-     
-    /*document.getElementById('videos').style.display = "none";*/ 
-    
-   invisivel();
+ 
+
 }
- function invisivel(){
-     /*alert(`o valor de display ${musica.style.display}`);*/
-     if(musica.style.display  === "none"){
-          /*alert('entrou em === none');*/
-          musica.style.display  = "block"; 
-          document.getElementById('videos').hidden=false; 
-          document.body.ifrm.hidden = false;
-     } else {
-         /* alert('entrou em === block');*/
-          musica.style.display  = "none";
-          document.getElementById('iframe#videos').hidden=true;
-          musica.style.hidden  = true;  
-          document.body.ifrm.hidden = true;
-     }   
-    /* if(x==1) {
-          alert(`o valor de x ${x}`)
-       
-          document.body.style.getElementById('videos').display = "block" 
-     }else {
-          document.body.style.getElementById('videos').display = "none" 
-          alert(`o valor de x = ${x}`)
-     }*/   
-     /*document.getElementById('videos').style.display = "none";*/
-
- }
-/*function tocar_video(mysrc){
-	var video = document.createElement('video');
-
-	video.src = mysrc;
-	video.autoplay = true;
-}
-*/
-
-/* ifrm = document.createElement("iframe#videos");
-    ifrm.setAttribute("src", "src="https://www.youtube.com/embed/1P2e0kwkIn4"");
-    ifrm.style.width =560+"px";
-    ifrm.style.height =  315+"px";
-    document.body.appendChild(ifrm);
-  /*  <iframe width="560" height="315" src="https://www.youtube.com/embed/1P2e0kwkIn4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */
